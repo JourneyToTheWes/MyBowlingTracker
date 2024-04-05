5454e00ec8a7759d2c2f034e0bca456d5b2c303a
Wesley Wei <wesley631w@gmail.com>
2024-04-05 13:41:48 -0400

    Implemented logic to parse bowling game score logic into a dictionary - Scraping all scores and putting dict into bowling_scores array - Added comments - Removed vestigial code/comments

--- scripts/syncPassportScoreScraper.py
+++ scripts/syncPassportScoreScraper.py
@@ -1,4 +1,3 @@
-from urllib.request import urlopen
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,38 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
+import re
 
 # Script arguments
 email = sys.argv[1]
 password = sys.argv[2]
-
-url = "https://www.syncpassport.com/MyScores"
-
-# page = urlopen(url)
-# html_bytes = page.read()
-# html = html_bytes.decode("utf_8")
-# print(html)
-
-# login
-# import requests
-login_url = "https://www.syncpassport.com/SignUp/SignIn"
-# login_payload = {
-#     "Email": "some-email",
-#     "Password": "some-password"
-# }
-# s = requests.session()
-# response = s.post(login_url, login_payload)
-# print(response.status_code)
-# print(response.content)
 
 # Create Chrome web driver
 option = webdriver.ChromeOptions()
 option.add_argument("start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 
-driver.implicitly_wait(3)
-
 # Login to Sync Passport
+login_url = "https://www.syncpassport.com/SignUp/SignIn"
 driver.get(login_url)
 driver.find_element(By.ID, "Email").send_keys(email)
 driver.find_element(By.ID, "Password").send_keys(password)
@@ -51,8 +31,67 @@ driver.get(score_url)
 driver.find_element(By.ID, "FilterDate").click()
 driver.find_element(By.ID, "clearDateFilter").click() # clear date filter to list all scores
 
-# Extract all score data using Beautiful Soup
+def extract_HTML_bowling_game_score(html_game):
+    """
+    Extracts a BeautifulSoup object of the SyncPassport bowling game
+    structured within a div of class "clearfix loadingBackground". The
+    game is structured and returned as a dictionary.
+ 
+    Args:
+        html_game (BeautifulSoup obj): Bowling game scraped from div on SyncPassport
+ 
+    Returns:
+        dict: the game stats (date, bowling center, and score)
+    """
+    bowling_score_dict = {}
 
+    # Get date
+    date = html_game.find('h2', class_='scoredate').text
+    bowling_score_dict["date"] = date
+
+    # Get bowling center
+    bowling_center = html_game.find(class_='scorecenter').text
+    bowling_score_dict["bowling_center"] = bowling_center
+
+    # Get all bowling frame HTML markup
+    cls_frame_regex = re.compile('^cls_frame(10)?$')
+    frames = html_game.find_all('td', { "class": cls_frame_regex })
+
+    frame_stats = []
+
+    for frame_num, frame in enumerate(frames, start=1):
+        frame_stat = {}
+        frame_stat["frame_num"] = frame_num
+
+        # Get all frame counts (3rd count in tenth frame)
+        frame_counts = frame.find_all('td', { "class": re.compile('^cls_ball[123]$') })
+
+        if (len(frame_counts) >= 2):
+            frame_count_1 = frame_counts[0].text
+            frame_count_2 = frame_counts[1].text
+            frame_stat["frame_count_1"] = frame_count_1
+            frame_stat["frame_count_2"] = frame_count_2
+
+            if (len(frame_counts) == 3):
+                frame_count_3 = frame_counts[2]
+                frame_stat["frame_count_3"] = frame_count_3.text
+
+            # Get frame score
+            frame_score = frame.find('td', { "class": re.compile('^cls_framescore(10)?') }).text
+            frame_stat["frame_score"] = frame_score
+
+            frame_stats.append(frame_stat)
+
+    bowling_score_dict["frame_stats"] = frame_stats
+
+    # Get game total score
+    score_total = html_game.find('td', class_='cls_scoretotal').text
+    bowling_score_dict["score_total"] = score_total
+
+    print(bowling_score_dict)
+    return bowling_score_dict
+
+# Extract all score data using Beautiful Soup
 try:
     # Wait until score spinner disappears
     wait = WebDriverWait(driver, 15).until(EC.invisibility_of_element_located((By.CLASS_NAME, "spinner")))
@@ -60,9 +99,12 @@ try:
     soup = BeautifulSoup(driver.page_source, "html.parser")
     div_scores = soup.find(id="scoreSheetResults")
 
-    for child in div_scores.find_all('div', class_='clearfix loadingBackground'):
-        print(child)
-        break
+    bowling_scores = []
+    for html_game in div_scores.find_all('div', class_='clearfix loadingBackground'):
+        bowling_score_dict = extract_HTML_bowling_game_score(html_game)
+        bowling_scores.append(bowling_score_dict)
+        f = open('scores.json', 'w')
+        f.write(str(bowling_scores))
+        f.close()
 except TimeoutException:
     print("Scores took too long to load")
-    
